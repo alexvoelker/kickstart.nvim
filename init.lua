c596e69bb50302b434e0825fb507062cff8d667d
Alex <25597637+alexvoelker@users.noreply.github.com>
2023-10-14 23:58:01 -0400

    added lsp for golang

--- init.lua
+++ init.lua
@@ -488,8 +488,8 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   clangd = {},
-  -- gopls = {},
-  pyright = { filetypes = { 'py' } },
+  gopls = {},
+  pyright = {},
   rust_analyzer = {},
   -- tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
